cf8ec1ff19baa2db439b45758a0bc2e7429a543a
labocho <labocho@penguinlab.jp>
2017-07-04 16:32:06 +0900

    support mail gem 2.5.5, 2.6.6

--- lib/maillog/delivery_method.rb
+++ lib/maillog/delivery_method.rb
@@ -3,14 +3,25 @@ require "mail/check_delivery_params"
 module Maillog
   class DeliveryMethod
     attr_accessor :settings
-    include ::Mail::CheckDeliveryParams
+
+    # >= 2.5.5, >= 2.6.6
+    if ::Mail::CheckDeliveryParams.respond_to?(:check)
+      private def open_envelope(mail)
+        ::Mail::CheckDeliveryParams.check(mail)
+      end
+    else
+      include ::Mail::CheckDeliveryParams
+      private def open_envelope(mail)
+        check_delivery_params(mail)
+      end
+    end
 
     def initialize(settings = {})
       @settings = settings.dup
     end
 
     def deliver!(mail)
-      smtp_envelope_from, smtp_envelope_to, message = check_delivery_params(mail)
+      smtp_envelope_from, smtp_envelope_to, message = open_envelope(mail)
       bcc = mail.bcc.blank? ? nil : mail.bcc.join(", ")
       log = log_mail(message, bcc)
       log.deliver
